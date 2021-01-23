38b8231e9760959d06ea4e08faa93fbf57dca9be
Afo Oshun <afoyenyen@Afos-iMac.local>
2021-01-23 23:03:18 +0000

    importing images

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { Fragment } from 'react';
+import { render } from 'react-dom';
+import { GlobalStyles } from './global-styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+	<Fragment>
+		<GlobalStyles />
+		<App />
+	</Fragment>,
+
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
