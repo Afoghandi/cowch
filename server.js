7bfd369f70d0f473dfd6dbdaff76408c2f690ac1
afoghandi <ralphaeloshun@gmail.com>
2024-11-01 17:25:41 +0000

    update Mongo Auth

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5100;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
